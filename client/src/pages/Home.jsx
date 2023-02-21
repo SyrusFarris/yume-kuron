62b6f30fe4cb2c0798099966b47b96daab89f571
Syrus <syrusfarris@gmail.com>
2023-02-21 17:03:51 -0600

    created RenderCards functional component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,16 @@ import React, { useState, useEffect } from 'react';
 
 import { Loader, Card, FormField } from '../components';
 
+const RenderCards = ({data, title }) => {
+    if(data?.length > 0) {
+        return data.map((post) => <Card key={post._id} {...post} />)
+    }
+
+    return(
+        <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">{title}</h2>
+    )
+}
+
 const Home = () => {
     const [loading, setLoading] = useState(false);
     const [allPost, setAllPosts] = useState(null);
@@ -28,10 +38,21 @@ const Home = () => {
                 <>
                     {searchText && (
                         <h2 className="font-medium text-[#666e75] text-xl mb-3">
-                        Showing results for <span className="text-[#222328]">{searchText}</span></h2>
+                        Showing results for <span className="text-[#222328]">{searchText}</span>
+                        </h2>
                     )}
                     <div className="grid lg:grid-cols-4 sm:grid-col-3 xs:grid-cols-2 grid-cols-1 gap-3">
-
+                        {searchText ? (
+                            <RenderCards
+                            data={[]}
+                            title=" No search results found"
+                            />
+                        ) : (
+                            <RenderCards
+                            data={[]}
+                            title="No posts found"
+                            />
+                        )}
                     </div>
                 </>
             )}
